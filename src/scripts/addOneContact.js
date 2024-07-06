5a5c9ea29c38c86b9a34b09c9b356a50b668a08a
Shnurkova <Annshnur@gmail.com>
2024-07-06 13:16:00 +0300

    add one contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,21 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+const addOneContact = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf8');
+        const contacts = JSON.parse(data);
 
+        const newContact = createFakeContact();
+        contacts.push(newContact);
+
+        await fs.writeFile(PATH_DB, JSON.stringify(contacts), 'utf8');
+    } catch(err) {
+        console.error(err);
+    }
+};
 addOneContact();
+
+export { addOneContact };
+
